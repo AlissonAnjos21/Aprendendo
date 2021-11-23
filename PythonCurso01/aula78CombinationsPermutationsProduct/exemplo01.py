7d51372012bed50123039e20c4c236a2f0f12ed7
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2021-11-23 14:06:58 -0300

    combinacoespermutacoesprodutos

---
+++ PythonCurso01/aula78CombinationsPermutationsProduct/exemplo01.py
@@ -0,0 +1,4 @@
+# Combinações, Permutações e Produto
+# Combinação - A ordem não importa, os valores únicos NÃO são repetidos
+# Permutação - A ordem importa, os valores únicos NÃO são repetidos
+# Produto - A ordem importa, os valores únicos SÃO repetidos
