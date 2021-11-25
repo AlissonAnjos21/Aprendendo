8e01ed2acd2220f68eb5b1398ec5c674d9ce900b
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2021-11-25 10:16:12 -0300

    exemplo3

--- PythonCurso01/aula82Reduce/exemplo03.py
+++ PythonCurso01/aula82Reduce/exemplo03.py
@@ -2,4 +2,6 @@ from dados import pessoas
 from functools import reduce
 
 soma_idades = reduce(lambda ac, x: x['idade'] + ac, pessoas, 0)
-media_idades = soma_idades / len(soma_idades)
+media_idades = soma_idades / len(pessoas)
+
+print(f'A soma das idades é: {soma_idades}\nA média das idades é: {media_idades}')
