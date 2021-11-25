c1749e7201c125e8db90d2735b3e3475505a46cd
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2021-11-25 10:12:00 -0300

    exemplo2

--- PythonCurso01/aula82Reduce/exemplo02.py
+++ PythonCurso01/aula82Reduce/exemplo02.py
@@ -1,4 +1,9 @@
 from dados import produtos
 from functools import reduce
 
+total_preco = reduce(lambda ac, x: x['valor'] + ac, produtos, 0)
 
+# Caso eu queira saber a média.
+media = total_preco / len(produtos)
+
+print(f'O total dos preços é: {total_preco}\nA média dos preços é: {media}')
--- PythonCurso01/aula82Reduce/exemplo03.py
+++ PythonCurso01/aula82Reduce/exemplo03.py
@@ -1,4 +1,5 @@
 from dados import pessoas
 from functools import reduce
 
-
+soma_idades = reduce(lambda ac, x: x['idade'] + ac, pessoas, 0)
+media_idades = soma_idades / len(soma_idades)
