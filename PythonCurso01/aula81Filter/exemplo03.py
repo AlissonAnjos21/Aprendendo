77d689f7075f171a9359720956e7255d33c6053e
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2021-11-25 09:39:13 -0300

    exemploNovo

--- PythonCurso01/aula81Filter/exemplo03.py
+++ PythonCurso01/aula81Filter/exemplo03.py
@@ -1,10 +1,13 @@
 from dados import pessoas
 
 def e_maior(x):
-    if x >= 18:
+    if x['idade'] >= 18:
         return True
     else:
         return False
 
 # também é possível usar uma função comum, porém o recomendado é que ela retorne True ou False, caso contrário é uma boa prática usar a função map.
 maior_idade = filter(e_maior, pessoas)
+
+for idade in maior_idade:
+    print(idade)
