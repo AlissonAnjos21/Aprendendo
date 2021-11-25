8d569dbbc1e03d24b0d47ed411c8ef1b7c13ab1a
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2021-11-25 09:42:58 -0300

    arrumando

--- PythonCurso01/aula81Filter/exemplo03.py
+++ PythonCurso01/aula81Filter/exemplo03.py
@@ -6,7 +6,7 @@ def e_maior(x):
     else:
         return False
 
-# também é possível usar uma função comum, porém o recomendado é que ela retorne True ou False, caso contrário é uma boa prática usar a função map.
+# Também é possível usar uma função comum, porém o recomendado é que ela retorne True ou False, caso contrário é uma boa prática usar a função map.
 maior_idade = filter(e_maior, pessoas)
 
 for idade in maior_idade:
