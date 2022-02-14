3b7fed49ef20879de85ac4b1cd05a89e84db4de7
Jurandir <alissondoran@gmail.com>
2022-02-14 14:40:51 -0300

    diretorioLocal

--- PythonCurso01/aula89CriandoLendoEscrevendoApagandoArquivos/exemplo01.py
+++ PythonCurso01/aula89CriandoLendoEscrevendoApagandoArquivos/exemplo01.py
@@ -1,3 +1,13 @@
+# Entra na pasta onde está este arquivo, caso contrário ele faria tudo na pasta principal
+import os
+diretorio_geral = os.path.dirname(__file__)
+diretorio_local = 'parte01/texto01.txt'  # Local e nome do arquivo que eu quero criar
+juntando_os_caminhos_do_diretorio_e_nome_do_arquivo_que_sera_criado = os.path.join(diretorio_geral, diretorio_local)
+
 # Criar, ler, escrever e apagar arquivos
 
+arquivo = open(juntando_os_caminhos_do_diretorio_e_nome_do_arquivo_que_sera_criado, 'w+')  # Write (escreve) + Leitura
 
+arquivo.write('Primeira Linha\n')
+arquivo.write('Segunda Linha\n')
+arquivo.write('Terceira Linha\n')
