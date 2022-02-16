53289b557cf936de863e2a932993e28cdae9151d
Jurandir <simplesEmail@email.com>
2022-02-16 15:17:49 -0300

    json

--- PythonCurso01/aula89CriandoLendoEscrevendoApagandoArquivos/parte03/exemplo01.py
+++ PythonCurso01/aula89CriandoLendoEscrevendoApagandoArquivos/parte03/exemplo01.py
@@ -1,1 +1,30 @@
 # Arquivos JSON
+
+import json
+
+dicionario = {
+    'Usuario01': {
+        'nome': 'Jurandir',
+        'idade': 42,
+    },
+    'Usuario02': {
+        'nome': 'Valdir',
+        'idade': 21,
+    },
+    'Usuario03': {
+        'nome': 'Osvaldo',
+        'idade': '13',
+    }
+}
+
+dicionario_formato_json = json.dumps(dicionario, indent=True)  # Converte o dicionário para o formato JSON (o indent é opcional)
+
+import os
+diretorio_geral = os.path.dirname(__file__)
+diretorio_local = 'arquivoJSON.json'
+criacao_arquivo = os.path.join(diretorio_geral, diretorio_local)
+
+print(dicionario_formato_json)
+
+with open(criacao_arquivo, 'w+') as arquivo:
+    arquivo.write(dicionario_formato_json)
---
+++ PythonCurso01/aula89CriandoLendoEscrevendoApagandoArquivos/parte03/leitorJSON.py
@@ -0,0 +1,16 @@
+import json
+import os
+diretorio_geral = os.path.dirname(__file__)
+diretorio_local = 'arquivoJSON.json'
+criacao_arquivo = os.path.join(diretorio_geral, diretorio_local)
+
+with open(criacao_arquivo, 'r') as arquivo:
+    captura_json = arquivo.read()
+    print(captura_json)  # Ainda está em formato JSON
+
+    arquivo_convertido_dict = json.loads(captura_json)  # Voltou a ser um dicionário
+
+for chave, valor in arquivo_convertido_dict.items():
+    print(chave)
+    for chave1, valor1 in valor.items():
+        print(chave1, valor1)
