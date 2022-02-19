536a31572b0325cbb955328646a6917d3363c254
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-19 11:13:22 -0300

    decorando

--- PythonCurso01/aula91FuncoesDecoradorasDecoradores/exemplo01.py
+++ PythonCurso01/aula91FuncoesDecoradorasDecoradores/exemplo01.py
@@ -3,5 +3,17 @@
 def chefe(funcao):
     def empregado():
         funcao()
-        print('Executo qualquer coisa, trabalho muito bem :D')
+        print('Trabalho muito bem :D')
     return empregado  # Lembrando que o empregado não deve ser retornado executando
+
+
+def qualquer_funcao():
+    print('Sou uma função de exemplo')
+
+
+# Lembre-se também que a função que você envia para o chefe não pode estar sendo executada (Não pode ter parênteses)
+qualquer_funcao = chefe(qualquer_funcao)  # Estou decorando a função "qualquer_funcao" com a função "chefe", ou seja, antes de executar a "qualquer_funcao", ela sempre passará dentro da função "chefe" antes
+
+qualquer_funcao()  # Executando fica nítido que ela passou dentro da "chefe" porque ela executou o print que existe em "empregado"
+
+
