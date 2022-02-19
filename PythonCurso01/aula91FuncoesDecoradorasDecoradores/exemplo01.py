a3c93cb8059a7d1959c1c8dcdf656985dfbfbce5
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-19 10:59:44 -0300

    teste

--- PythonCurso01/aula91FuncoesDecoradorasDecoradores/exemplo01.py
+++ PythonCurso01/aula91FuncoesDecoradorasDecoradores/exemplo01.py
@@ -1,1 +1,7 @@
 # Funções decoradoras e decoradores
+
+def chefe(funcao):
+    def empregado():
+        funcao()
+        print('Executo qualquer coisa, trabalho muito bem :D')
+    return empregado  # Lembrando que o empregado não deve ser retornado executando
