9d485397379467d18af5d397c059903f748e9de2
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-19 16:32:26 -0300

    kwargsFinal

--- PythonCurso01/aula53ArgsKwargsDesempacotamento/kwargs/exemplo03.py
+++ PythonCurso01/aula53ArgsKwargsDesempacotamento/kwargs/exemplo03.py
@@ -1,3 +1,9 @@
 # Capturando um determinado elemento kwargs
 
+def fala_nome_e_sobrenome(**kwargs):
+    nome = kwargs.get('nome')
+    sobrenome = kwargs.get('sobrenome')
+    print(sobrenome, nome)
 
+
+fala_nome_e_sobrenome(nome='Seiya', sobrenome='Ryuuguuin')
