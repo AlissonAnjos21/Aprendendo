bbed1451096cb2f262c0faa3ce4c8695d14eb067
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-20 12:25:21 -0300

    tentativa

--- PythonCurso01/desafiosExercicios/desafioUndoRedo.py
+++ PythonCurso01/desafiosExercicios/desafioUndoRedo.py
@@ -7,16 +7,22 @@ Faça uma lista de tarefas com as seguintes opções:
 
 """
 
-
-# tarefas.append(input('Informe uma tarefa: \n'))
-
-# for tarefa in tarefas:
-    #  print(tarefa)
-
+from time import sleep
 
 tarefas = []
 while True:
     
     opcao = input('\nO quê você gostaria de fazer?\nDigite:\n1 - Para Adicionar Uma Tarefa\n2 - Para Listar as Tarefas\n3 - Para Desfazer a Última Ação\n4 - Para Refazer Uma Ação Desfeita\n\n')
 
-    
+    if opcao == '1':
+        tarefas.append(input('\nInforme uma tarefa: \n'))
+
+    if opcao == '2' and tarefas == []:
+        print('\nA lista de tarefas está vazia, tente adicionar uma tarefa primeiro.')
+    elif opcao == '2' and tarefas != []:
+        print('\nEssas são todas as tarefas: ')
+        for tarefa in tarefas:
+            print(tarefa)
+            sleep(0.5)
+
+    sleep(1.5)
