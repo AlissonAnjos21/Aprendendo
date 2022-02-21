dd8525d9e9cf590d99d267531d7c738151bd2d7d
Jurandir <simplesEmail@email.com>
2022-02-21 15:10:09 -0300

    melhora

--- PythonCurso01/desafiosExercicios/validarCnpj/cnpj.py
+++ PythonCurso01/desafiosExercicios/validarCnpj/cnpj.py
@@ -1,18 +1,3 @@
 import re
 
 
-def numerificar(cnpj_usuario):
-    # Substitui por '' (vazio) qualquer coisa que não estiver entre 0 e 9, no cnpj_usuario 
-    return re.sub(r'[^0-9]', '', cnpj_usuario)
-
-
-def forma_lista(cnpj_usuario, lista_cnpj):
-    if len(cnpj_usuario) != 14:
-        print('\nEste CNPJ não é válido!\n')
-        return
-    else:
-        for v in range(len(cnpj_usuario) - 2):
-            lista_cnpj.append(int(cnpj_usuario[v]))
-        return lista_cnpj
-
-
