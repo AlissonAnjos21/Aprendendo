98aece957ecfc3ad0b56205a3769ce363bbd30d5
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-23 21:31:18 -0300

    escopoVariaveis

--- PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo03/animal.py
+++ PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo03/animal.py
@@ -1,4 +1,17 @@
 class Animal:
     # O atributo self sempre deve ser o primeiro no momento da declaração dos argumentos, porém quando se trata de fornecer estes argumentos, o mesmo não é necessário ser recebido
     def __init__(self, nome, idade, dormindo=False, comendo=False):
-        pass
+        
+        # Apesar de possuirem o mesmo nome, o self.nome representa ao atributo nome que existe nesta classe, enquanto o argumento nome representa apenas o valor que foi enviado durante a criação da instância 
+        self.nome = nome
+        self.idade = idade
+        self.dormindo = dormindo
+        self.comendo = comendo
+
+        # É possível criar, sem o self, variáveis neste método, porém elas só estarão disponíveis no escopo do método
+        teste = 'Teste'
+
+    def metodo_exemplo_teste(self):
+        # print(teste) - Erro!!! O teste está fora do escopo do método
+        print(self.nome)  # Funciona perfeitamente
+
--- PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo03/main.py
+++ PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo03/main.py
@@ -1,4 +1,8 @@
 from animal import Animal
 
-a1 = Animal()
-a2 = Animal()
+a1 = Animal('Vaca', 1)  # a1.nome = 'Vaca', a1.idade = 1
+a2 = Animal('Humano', 21)  # a2.nome = 'Humano', a2.idade = 21
+
+print(a1.nome)
+
+a2.metodo_exemplo_teste()
