0483487db1da75b546e315a5421c01b5b56ba496
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-23 21:12:40 -0300

    maisExemplo

--- PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo03/animal.py
+++ PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo03/animal.py
@@ -1,2 +1,4 @@
 class Animal:
-    pass
+    # O atributo self sempre deve ser o primeiro no momento da declaração dos argumentos, porém quando se trata de fornecer estes argumentos, o mesmo não é necessário ser recebido
+    def __init__(self, nome, idade, dormindo=False, comendo=False):
+        pass
