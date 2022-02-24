77c5a38adecfb5dd598cb18a6cd272b4741cb471
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-23 21:32:54 -0300

    base

---
+++ PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo04/animal.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, nome, idade, dormindo=False, comendo=False):
+        self.nome = nome
+        self.idade = idade
+        self.dormindo = dormindo
+        self.comendo = comendo
+
+        
+        
---
+++ PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo04/main.py
@@ -0,0 +1,6 @@
+from animal import Animal
+
+a1 = Animal('Vaca', 1)  # a1.nome = 'Vaca', a1.idade = 1
+a2 = Animal('Humano', 21)  # a2.nome = 'Humano', a2.idade = 21
+
+
