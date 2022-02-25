b61540173dd0bc6ce30269634e3998959cd2562e
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-24 22:41:35 -0300

    metodos_da_classe

--- PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo06/casa.py
+++ PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo06/casa.py
@@ -1,2 +1,22 @@
 class Casa:
-    pass
+    ano_atual = 2022
+
+
+    def __init__(self, numero, tempo_uso):
+        self.numero = numero
+        self.tempo_uso = tempo_uso
+    
+    
+    # Método convencional, ou seja, método referente à instância
+    def get_ano_entrada(self):
+        print(self.ano_atual - self.tempo_uso)
+
+
+    # Método da classe, refere-se a classe como um todo, e não aos objetos específicos
+    # Ao invés do orgumento nomeado como "self", nomeia-se, por convenção, o argumento como "cls" para ilustrar a ideia de que ele é referente a classe
+    # Para que ele se torne realmente um método da classe, é necessário decorá-lo com o @classmethod
+    @classmethod
+    def tempo_uso_pelo_ano_entrada(cls, numero, ano_entrada):
+        # Perceba que "tempo_uso" neste caso é apenas uma variável local deste escopo
+        tempo_uso = cls.ano_atual - ano_entrada
+        return cls(numero, tempo_uso)
--- PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo06/main.py
+++ PythonCurso01/aula98ProgramacaoOrientadaObjeto/exemplo06/main.py
@@ -1,3 +1,16 @@
 # Métodos de Classe
+# São métodos que são referentes a classe em si, e não ao objeto instanciado
 
-import casa
+from casa import Casa
+
+c1 = Casa('N°21', 12)
+print(c1.numero, c1.tempo_uso)  # Obtemos o mesmo resultado do "tempo_uso" que foi informada durante a criação
+c1.get_ano_entrada()
+
+print()
+print()
+print()
+
+c2 = Casa.tempo_uso_pelo_ano_entrada('N°42', 2001)
+print(c2.numero, c2.tempo_uso)
+c2.get_ano_entrada()  # Obtemos o mesmo resultado do que foi informado durante a criação
