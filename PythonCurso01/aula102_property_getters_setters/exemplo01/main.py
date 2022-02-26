85355608fc56c55f44c44e1bb6847687255c8764
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-26 15:16:56 -0300

    getter

--- PythonCurso01/aula102_property_getters_setters/exemplo01/main.py
+++ PythonCurso01/aula102_property_getters_setters/exemplo01/main.py
@@ -1,3 +1,18 @@
 # @property: Getters e Setters
 
+class Carro:
+    def __init__(self, modelo, preco):
+        self.modelo = modelo
+        self.preco = preco
+    
 
+    def promocao(self, desconto):
+        self.preco = self.preco - (self.preco * desconto / 100)
+
+    # Getter
+    # Precisa ser decorado com @property
+    # Precisa possuir o nome do atributo ao qual ele é referente
+    # O que é retornado precisa, por convenção, ser o nome do atributo precedido por "_"
+    @property
+    def preco(self):
+        return self._preco
