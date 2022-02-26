9004e8505f0881f7a9fca9b97e2853c2a7e7ebdf
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-02-26 15:36:14 -0300

    primeiro_exemplo

--- PythonCurso01/aula102_property_getters_setters/exemplo01/main.py
+++ PythonCurso01/aula102_property_getters_setters/exemplo01/main.py
@@ -9,10 +9,31 @@ class Carro:
     def promocao(self, desconto):
         self.preco = self.preco - (self.preco * desconto / 100)
 
-    # Getter
+    # Getter:
     # Precisa ser decorado com @property
     # Precisa possuir o nome do atributo ao qual ele é referente
     # O que é retornado precisa, por convenção, ser o nome do atributo precedido por "_"
     @property
     def preco(self):
         return self._preco
+
+
+    # Setter:
+    # Precisa ser decorado como (nome_do_atributo).setter
+    # Precisa possuir o nome do atributo ao qual ele é referente
+    # Precisa utilizar self.(nome_do_valor_retornado_pelo_getter)
+    @preco.setter
+    def preco(self, valor):
+        # Métodos setters são úteis para tratar os valores antes de atribuí-los
+        if isinstance(valor, str):
+            # Desta maneira eu evito de receber uma string e/ou um possível "R$"
+            valor = float(valor.replace('R$', ''))
+
+        self._preco = valor
+
+
+c1 = Carro('Ferrari', 12100000)
+print(c1.preco)
+
+c2 = Carro('Corsa', 'R$12000')
+print(c2.preco)
