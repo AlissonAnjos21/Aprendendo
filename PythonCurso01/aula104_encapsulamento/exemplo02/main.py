e7dc5a9a94f34b00d8f8bc86086ca8cc4bba7f97
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-03-05 20:29:23 -0300

    exemplo_2

---
+++ PythonCurso01/aula104_encapsulamento/exemplo02/main.py
@@ -0,0 +1,31 @@
+class Carro:
+    def __init__(self):
+        self.lista_carros = {}
+
+
+    def inserir_carro(self, id, nome):
+        if 'carros' in self.lista_carros:
+            self.lista_carros['carros'].update({id:nome})
+        else:
+            self.lista_carros['carros'] = {id:nome}
+
+
+    def remover_carro(self, id):
+        del self.lista_carros['carros'][id]
+    
+
+    def mostrar_carros(self):
+        for id, nome in self.lista_carros.items():
+            print(id, nome)
+
+
+c1 = Carro()
+c1.inserir_carro(1, 'Corsa')
+c1.inserir_carro(2, 'Mustang')
+c1.inserir_carro(3, 'Camaro')
+
+c1.lista_carros = "Vou quebrar essa classe toda 🤡"
+
+# A classe foi quebrada pois a "lista_carros" não é mais um dicionário
+c1.remover_carro(1)
+c1.mostrar_carros()
