09ae758e1def243159ed349753258975236e4a34
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-03-08 06:19:35 -0300

    exemplo_1

--- PythonCurso01/aula111_sobreposicao_de_membros/exemplo01/exemplo01.py
+++ PythonCurso01/aula111_sobreposicao_de_membros/exemplo01/exemplo01.py
@@ -16,6 +16,10 @@ class Professor(Pessoa):
 
 # Estou herdando a classe Professor diretamente e herdando a classe Pessoa por tabela
 class ProfessorMestre(Professor):
-    
+    # Estou sobrepondo o método ensinar de professor que já possui esse nome
     def ensinar(self):
-        print(f'{self.nome_classe} Está Ensinando')
+        print(f'{self.nome_classe} Está Ensinando Como um Mestre')
+
+
+p1 = ProfessorMestre('Maria', 34)
+p1.ensinar()
