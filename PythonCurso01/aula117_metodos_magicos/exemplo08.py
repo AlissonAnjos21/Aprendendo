53278e40a9c7ea3db31f92de932b7cc4e5dda5c2
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-03-17 19:29:52 -0300

    exemplo_8_1

--- PythonCurso01/aula117_metodos_magicos/exemplo08.py
+++ PythonCurso01/aula117_metodos_magicos/exemplo08.py
@@ -2,6 +2,7 @@ class Exemplo:
     def __init__(self):
         pass
 
+    # Não é muito recomendável usar este método, pois a documentação do Python informa que ele nem sempre é chamado
     def __del__(self):
         print('Executo sempre que o Garbage Collector do Python é chamado, ou seja, no fim da execução')
 
