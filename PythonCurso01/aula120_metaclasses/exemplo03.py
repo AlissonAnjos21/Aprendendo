8fb310b6dbd090fbdfa86b3fc728178cfe5c1eba
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-03-31 02:33:16 -0300

    exemplo_3

---
+++ PythonCurso01/aula120_metaclasses/exemplo03.py
@@ -0,0 +1,20 @@
+# Utilizando o type() para criar classes
+
+# Método convencional
+class Pessoa:
+    def respirar(self):
+        print('Olá, estou respirando!')
+
+# Método diferenciado, com o type
+ClassePai = type(
+    # Nome da classe, classes a qual ela herda, atributos/métodos de classe respectivamente
+    'ClassePai', 
+    (Pessoa,),
+     {
+        'atributo_da_classe': 'Sou um atributo!'
+    }
+)
+
+classe = ClassePai()
+print(type(classe))  # <class '__main__.ClassePai'>
+classe.respirar()
