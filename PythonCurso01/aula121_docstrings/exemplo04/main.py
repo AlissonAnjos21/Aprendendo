d861c5a778d88cc2e4ffeda94895a73ac163eee7
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-03 00:48:39 -0300

    forma_alternativa

--- PythonCurso01/aula121_docstrings/exemplo04/main.py
+++ PythonCurso01/aula121_docstrings/exemplo04/main.py
@@ -4,3 +4,6 @@ help(funcoes_2)
 
 # Vendo a documentação de apenas uma função do módulo
 help(funcoes_2.soma)
+
+# Forma alternativa de se chamar a documentação de uma função
+print(funcoes_2.soma.__doc__)
---
+++ PythonCurso01/aula121_docstrings/exemplo05/main.py
@@ -0,0 +1,3 @@
+import classes
+
+help(classes)
