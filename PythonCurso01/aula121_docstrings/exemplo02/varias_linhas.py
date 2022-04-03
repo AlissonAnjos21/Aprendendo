d40c3b10e8e192c6615ec6e85021b1e8110c581a
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-02 23:55:03 -0300

    exemplo_2

---
+++ PythonCurso01/aula121_docstrings/exemplo02/varias_linhas.py
@@ -0,0 +1,17 @@
+"""Várias linhas de documentação.
+
+Após fazer uma descrição simples e rápida, você deixa uma linha em branco e começa a fazer
+a descrição detalhada. Esse texto é um perfeito exemplo disso, existe umapequena descrição, como consta acima,
+e após isso eu pulei uma linha e comecei a escrever a parte detalhada, ou seja, isso que você está lendo.
+
+Quis irure veniam culpa laboris tempor aliqua cillum magna irure aute aliquip Lorem. Lorem qui veniam
+reprehenderit exercitation fugiat ipsum cillum sit consequat sunt amet excepteur veniam. Sit ad dolore quis 
+consectetur occaecat dolor adipisicing qui. Sint dolore quis proident labore labore est reprehenderit. Quis 
+commodo ut ex aute proident ad consectetur voluptate ut qui. Lorem excepteur incididunt nisi cupidatat labore 
+laboris est.
+"""
+
+variavel = 'valor'
+
+def funcao():
+    return 1
