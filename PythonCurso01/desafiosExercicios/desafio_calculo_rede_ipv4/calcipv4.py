e8b465d2f4c3a8f844f0f62ca85fa1787bfb7948
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-05 06:18:52 -0300

    comeco

--- PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/calcipv4.py
+++ PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/calcipv4.py
@@ -1,3 +1,30 @@
 class CalcIpv4:
-    def __init__(self):
-        pass
+    def __init__(self, ip, mascara=None, cidr=None):
+        self.ip = ip
+        self.mascara = mascara
+        self.cidr = cidr
+
+    @property
+    def ip(self):
+        return self._ip
+    
+    @ip.setter
+    def ip(self, ip):
+        self._ip = ip
+
+    @property
+    def mascara(self):
+        return self._mascara
+
+    @mascara.setter
+    def mascara(self, mascara):
+        self._mascara = mascara
+
+    @property
+    def cidr(self):
+        return self._cidr
+
+    @cidr.setter
+    def cidr(self, cidr):
+        self._cidr = cidr
+    
