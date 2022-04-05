2df3ce88d0d9c538c37e5959219c8a74bbb9349b
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-05 20:56:02 -0300

    convert_to_binary

--- PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/calcipv4.py
+++ PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/calcipv4.py
@@ -3,7 +3,8 @@ class CalcIpv4:
         self.ip = ip
         self.mascara = mascara
         self.cidr = cidr
-        print(self.ip)
+        # print(self.ip)
+        # print(self.convert_to_binary(self.ip))
 
     @property
     def ip(self):
@@ -12,4 +13,20 @@ class CalcIpv4:
     @ip.setter
     def ip(self, ip):
         ip = ip.split('.')
+        for i, j in enumerate(ip):
+            ip[i] = int(j)
         self._ip = ip
+
+    def convert_to_binary(self, ip):
+        values = [128, 64, 32, 16, 8, 4, 2, 1]
+        bin_values = []
+        for i in ip:
+            bin_individual_value = []
+            for j in values:
+                if i >= j:
+                    bin_individual_value.append(1)
+                    i = i - j
+                else:
+                    bin_individual_value.append(0)
+            bin_values.append(bin_individual_value)
+        return bin_values
