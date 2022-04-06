83bd5a09269a1beecb7b711e2dc52652fd8a006b
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-06 03:58:12 -0300

    text_formatation

--- PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/main.py
+++ PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/main.py
@@ -3,8 +3,8 @@ from calcipv4 import CalcIpv4
 calcipv4 = CalcIpv4('192.168.0.25', '255.255.255.192', 26)
 
 print(f'IP: {calcipv4.format_decimal(calcipv4.ip, calcipv4.prefix)}')
-print(f'Máscara: {calcipv4.format_decimal(calcipv4.mask_value, calcipv4.prefix)}')
-print(f'Rede: {calcipv4.format_decimal(calcipv4.decimal_web_ip, calcipv4.prefix)}')
+print(f'Mask: {calcipv4.format_decimal(calcipv4.mask_value, calcipv4.prefix)}')
+print(f'Web: {calcipv4.format_decimal(calcipv4.decimal_web_ip, calcipv4.prefix)}')
 print(f'Broadcast: {calcipv4.format_decimal(calcipv4.decimal_broadcast_ip, calcipv4.prefix)}')
-print(f'Prefixo: {calcipv4.prefix}')
-print(f'Número de IPs da Rede: {calcipv4.number_of_ips}')
+print(f'Prefix: {calcipv4.prefix}')
+print(f'Number of Web IPs: {calcipv4.number_of_ips}')
