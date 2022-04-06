742a741760ec3e58262f77ff66317e3aedc3fe99
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-06 01:53:20 -0300

    solution_not_formated

--- PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/calcipv4.py
+++ PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/calcipv4.py
@@ -7,13 +7,28 @@ class CalcIpv4:
         self.mask = mask
         self.prefix = prefix
         
-        self.binary_ip = self.convert_ip_to_binary()
+        self.binary_ip = self.convert_ip_to_binary(self.ip)
         self.binary_mask = self.generate_binary_mask_from_prefix()
-        self.decimal_mask = self.convert_binary_mask_to_decimal()
-        print(self.ip)
-        print(self.binary_ip)
-        print(self.binary_mask)
-        print(self.decimal_mask)
+        self.decimal_mask = self.convert_binary_ip_to_decimal(self.binary_mask)
+        self.binary_broadcast_ip = self.generate_broadcast_ip()
+        self.binary_web_ip = self.generate_web_ip()
+        self.decimal_broadcast_ip = self.convert_binary_ip_to_decimal(self.binary_broadcast_ip)
+        self.decimal_web_ip = self.convert_binary_ip_to_decimal(self.binary_web_ip)
+        self.number_of_ips = self.generate_number_of_ips()
+        self.binary_mask_from_mask = self.convert_ip_to_binary(self.mask)
+        self.prefix = self.generate_prefix_from_binary_mask()
+        # print(self.ip)
+        # print(self.mask)
+        # print(self.binary_ip)
+        # print(self.binary_mask)
+        # print(self.decimal_mask)
+        # print(self.binary_broadcast_ip)
+        # print(self.binary_web_ip)
+        # print(self.decimal_broadcast_ip)
+        # print(self.decimal_web_ip)
+        # print(self.number_of_ips)
+        # print(self.binary_mask_from_mask)
+        # print(self.prefix)
 
     @property
     def ip(self):
@@ -26,9 +41,34 @@ class CalcIpv4:
             ip[i] = int(j)
         self._ip = ip
 
-    def convert_ip_to_binary(self):
+    @property
+    def mask(self):
+        return self._mask
+
+    @mask.setter
+    def mask(self, mask):
+        if mask:
+            mask = mask.split('.')
+            for i, j in enumerate(mask):
+                mask[i] = int(j)
+            self._mask = mask
+        else:
+            self._mask = None
+
+    @property
+    def prefix(self):
+        return self._prefix
+
+    @prefix.setter
+    def prefix(self, prefix):
+        if prefix:
+            self._prefix = prefix
+        else:
+            self._prefix = None
+
+    def convert_ip_to_binary(self, ip):
         bin_values = []
-        for i in self.ip:
+        for i in ip:
             bin_individual_value = []
             for j in self.values:
                 if i >= j:
@@ -52,13 +92,54 @@ class CalcIpv4:
                     bin_values.append(0)
             bin_mask.append(bin_values)
         return bin_mask
+
+    def generate_prefix_from_binary_mask(self):
+        prefix = 0
+        for i in self.binary_mask_from_mask:
+            for j in i:
+                if j == 1:
+                    prefix+=1
+        return prefix
+
                 
-    def convert_binary_mask_to_decimal(self):
-        decimal_mask = []
-        for i in self.binary_mask:
+    def convert_binary_ip_to_decimal(self, binary):
+        decimal_value = []
+        for i in binary:
             aux = 0
             for j, k in enumerate(i):
                 if k == 1:
                     aux += self.values[j]
-            decimal_mask.append(aux)
-        return decimal_mask
+            decimal_value.append(aux)
+        return decimal_value
+
+    def generate_broadcast_ip(self):
+        aux = 0
+        broadcast_ip = []
+        for i in self.binary_ip:
+            ip_value = []
+            for j in i:
+                if aux < self.prefix:
+                    ip_value.append(j)
+                    aux+=1
+                else:
+                    ip_value.append(1)
+            broadcast_ip.append(ip_value)
+        return broadcast_ip
+
+
+    def generate_web_ip(self):
+        aux = 0
+        web_ip = []
+        for i in self.binary_ip:
+            ip_value = []
+            for j in i:
+                if aux < self.prefix:
+                    ip_value.append(j)
+                    aux+=1
+                else:
+                    ip_value.append(0)
+            web_ip.append(ip_value)
+        return web_ip
+
+    def generate_number_of_ips(self):
+        return (2 ** (32 - self.prefix)) - 2
--- PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/main.py
+++ PythonCurso01/desafiosExercicios/desafio_calculo_rede_ipv4/main.py
@@ -1,3 +1,3 @@
 from calcipv4 import CalcIpv4
 
-c = CalcIpv4('10.20.12.45', prefix=26)
+c = CalcIpv4('10.20.12.45', '255.255.255.192', 26)
