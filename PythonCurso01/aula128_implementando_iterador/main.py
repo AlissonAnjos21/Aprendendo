09f7f0eb46f6c3476c165b510de64f05be14ee7d
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-04-08 22:01:43 -0300

    classe_lista

---
+++ PythonCurso01/aula128_implementando_iterador/main.py
@@ -0,0 +1,5 @@
+# Vou criar uma classe que terá as funcionalidades de uma lista
+# Isso é algo um tanto quanto redundante de se fazer
+# Isso servirá apenas para fins didáticos, e não deve ser implementado cotidianamente
+
+
