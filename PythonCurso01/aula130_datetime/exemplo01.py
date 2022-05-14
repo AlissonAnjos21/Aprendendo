369ea5881c187314a6f2787adfec6d381bf60d39
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-14 02:42:58 -0300

    date_to_str

---
+++ PythonCurso01/aula130_datetime/exemplo01.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+
+nova_data = datetime(2022, 5, 14, 2, 30, 20)  # Formato: ano, mês, dias, horas, minutos, segundos
+print('\nData Padrão:')
+print(nova_data)
+
+# Converte a data para o formato de str
+data_formatada = nova_data.strftime('%d/%m/%Y %H:%M:%S')  # as "/" e os ":" são opcionais, poderia ter qualquer coisa, ou até não ter nada
+print('\nData Formatada:')
+print(data_formatada)
